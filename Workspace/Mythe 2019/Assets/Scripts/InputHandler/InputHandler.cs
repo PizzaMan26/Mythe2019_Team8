2ad75440c7ce155c17b24b5b5816d344b11b9a5c
JudaHensen <24311@ma-web.nl>
2019-02-28 09:05:58 +0100

    Added Input Handler
    + Added Controller Support (For testing)
    + Added Keyboard Support (For testing)
    
    - Still need to create Android Support.

---
+++ Workspace/Mythe 2019/Assets/Scripts/InputHandler/InputHandler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+    public event Action Jump, Attack1, Attack2, Attack3;
+    public event Action<float> Walk;
+
+    // Check for input
+    void Update()
+    {   
+        // Check if the application is running on android
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            //if () Walk(value);
+            //if () Jump();
+            //if () Attack1();
+            //if () Attack2();
+        }
+        // Check if an controller is connected
+        else if (Input.GetJoystickNames().Length > 0)
+        {
+            Walk(Input.GetAxis("C_Horizontal"));
+            if (Input.GetButtonDown("C_Jump")) Jump();
+            if (Input.GetButtonDown("C_Attack1")) Attack1();
+            if (Input.GetButtonDown("C_Attack2")) Attack2();
+            if (Input.GetButtonDown("C_Attack3")) Attack3();
+        }
+        // If the application is not running on android, and there is no controller connected, use PC controls
+        else
+        {
+            Walk(Input.GetAxis("PC_Horizontal"));
+            if (Input.GetButtonDown("PC_Jump")) print("jump");
+            if (Input.GetButtonDown("PC_Attack1")) Attack1();
+            if (Input.GetButtonDown("PC_Attack2")) Attack2();
+            if (Input.GetButtonDown("PC_Attack3")) Attack3();
+        }
+    }
+}
---
+++ Workspace/Mythe 2019/Assets/Scripts/InputHandler/test.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    /*
+    This script is an example.
+    In this script you can see how the InputHandler script can be used.  
+    */
+    
+    private InputHandler inputHandler;
+
+    void Start()
+    {
+        inputHandler = GameObject.FindWithTag("InputHandler").GetComponent<InputHandler>();
+
+        //inputHandler.Jump += /*Zet hier een functie neer.*/;
+        //inputHandler.Attack1 += /*Zet hier een functie neer.*/;
+        //inputHandler.Attack2 += /*Zet hier een functie neer.*/;
+        //inputHandler.Attack3 += /*Zet hier een functie neer.*/;
+        //inputHandler.Walk += /*Zet hier een functie neer.*/;
+    }
+
+}
