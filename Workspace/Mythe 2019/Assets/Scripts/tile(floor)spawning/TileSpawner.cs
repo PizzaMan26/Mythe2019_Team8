7f359885cd5b94e4aad87fbcfd26b7bbe90a2515
BradleyBekker <thekid72gaming@gmail.com>
2019-02-28 09:26:30 +0100

    added level spawner adds new level when function Stage Up is called

---
+++ Workspace/Mythe 2019/Assets/Scripts/tile(floor)spawning/TileSpawner.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileSpawner : MonoBehaviour
+{
+    //array width all prefabs
+    public GameObject[] allLevels;
+    //list of loaded prefabs
+    private List<GameObject> activeLevels = new List<GameObject>();
+    //keeps track of index
+    int index = 0;
+
+    private void Start()
+    {
+        //picks 3 starting levels
+        for (int i = 0; i < 3; i++)
+        {
+            activeLevels.Add(allLevels[Random.Range(0, allLevels.Length)]);
+        }
+        //loads 3 starting levels
+        for (int i = 0; i < activeLevels.Count; i++)
+        {
+            Instantiate(activeLevels[i], new Vector3(0, 0 + 25 * i, 0), Quaternion.identity);
+            index++;
+        }
+    }
+    
+    /*void Update()
+    {
+        if (Input.anyKeyDown)
+            StageUp();
+    }*/
+
+    void StageUp()
+    {
+        //adds new level 
+        activeLevels.Add(allLevels[Random.Range(0, 3)]);
+        
+        //spawnes new level
+        Instantiate(activeLevels[index], new Vector3(0, 0 + 25 * index, 0), Quaternion.identity);
+        index++;
+        print(index);
+
+    }
+}
