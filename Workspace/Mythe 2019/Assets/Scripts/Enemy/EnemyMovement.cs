ccdfafd528d7510435d42f128dbd5a3e9a8c47cb
SMOOCHIE <justinudo959@gmail.com>
2019-03-01 13:21:43 +0100

    - Added: EnemyMovement - Added: Enemy following

---
+++ Workspace/Mythe 2019/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    private TestPlayerMovement tpm = new TestPlayerMovement();
+
+    public GameObject target;
+
+    private float _enemySpeed = 4f;
+    private int _enemyLayer = 0;
+
+    public int getEnemyLayer
+    {
+        get
+        {
+            return _enemyLayer;
+        }
+
+        set
+        {
+            _enemyLayer = value;
+        }
+    }
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        CheckForPlayer();
+    }
+
+    public void CheckForPlayer()
+    {
+        if(_enemyLayer == tpm.getLayer)
+        {
+            Vector3 pos = new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z);
+
+            transform.position = Vector3.MoveTowards(transform.position, pos, .05f);
+        }
+    }
+}
---
+++ Workspace/Mythe 2019/Assets/Scripts/Enemy/TestPlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPlayerMovement : MonoBehaviour
+{
+    //just for testing if the enemy follows the player when he walks somewhere else
+
+     private float speed = 6f;
+     private int _layer = 0;
+
+     public int getLayer
+     {
+         get
+         {
+            return _layer;
+         }
+     }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(-speed * Time.deltaTime, 0f, 0f);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(speed * Time.deltaTime, 0f, 0f);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(0f, speed * Time.deltaTime, 0f);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += new Vector3(0f, -speed * Time.deltaTime, 0f);
+        }
+    }
+}
