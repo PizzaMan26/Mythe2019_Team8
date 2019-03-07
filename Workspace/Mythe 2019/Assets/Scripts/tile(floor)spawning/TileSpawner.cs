bc00fb52c3ad5336e77e6b3044dca9c22a2ecca1
BradleyBekker <thekid72gaming@gmail.com>
2019-03-07 08:55:26 +0100

    turned tile spawner into tower like structure

--- Workspace/Mythe 2019/Assets/Scripts/tile(floor)spawning/TileSpawner.cs
+++ Workspace/Mythe 2019/Assets/Scripts/tile(floor)spawning/TileSpawner.cs
@@ -14,33 +14,100 @@ public class TileSpawner : MonoBehaviour
     private void Start()
     {
         //picks 3 starting levels
-        for (int i = 0; i < 3; i++)
+        for (int i = 0; i < 4; i++)
         {
             activeLevels.Add(allLevels[Random.Range(0, allLevels.Length)]);
         }
-        //loads 3 starting levels
+        //loads starting level
+        int temp = 1;
         for (int i = 0; i < activeLevels.Count; i++)
         {
-            Instantiate(activeLevels[i], new Vector3(0, 0 + 25 * i, 0), Quaternion.identity);
-            index++;
+            
+            switch (temp)
+            {
+                case 1:
+                    Instantiate(activeLevels[index], new Vector3(0, 0 + (activeLevels[index].transform.localScale.y * 0.25f) * index, 0), Quaternion.Euler(0f, -90f * index, 0f));
+                    temp++;
+                    break;
+
+                case 2:
+                    Instantiate(activeLevels[index], new Vector3(activeLevels[index].transform.localScale.x * 0.5f, 0 + (activeLevels[index].transform.localScale.y * 0.25f) * index, activeLevels[index].transform.localScale.x * 0.5f), Quaternion.Euler(0f, -90f * index, 0f));
+                    temp++;
+                    break;
+
+                case 3:
+                    print(temp);
+                    Instantiate(activeLevels[index], new Vector3(0, 0 + (activeLevels[index].transform.localScale.y * 0.25f) * index, 21), Quaternion.Euler(0f, -90f * index, 0f));
+                    temp++;
+                    break;
+            
+                case 4:
+                    print(temp);
+                    Instantiate(activeLevels[index], new Vector3(-(activeLevels[index].transform.localScale.x) * 0.5f, 0 + (activeLevels[index].transform.localScale.y * 0.25f) * index, activeLevels[index].transform.localScale.x * 0.5f), Quaternion.Euler(0f, -90f * index, 0f));
+                    temp++;
+                break;
+        }
+
+        if (temp % 5 == 0)
+                temp = 0;
+
+
+                index++;
+
         }
     }
     
-    /*void Update()
+    void Update()
     {
-        if (Input.anyKeyDown)
+        //if (Input.anyKeyDown)
             StageUp();
-    }*/
+    }
 
     void StageUp()
     {
         //adds new level 
-        activeLevels.Add(allLevels[Random.Range(0, 3)]);
-        
         //spawnes new level
-        Instantiate(activeLevels[index], new Vector3(0, 0 + 25 * index, 0), Quaternion.identity);
-        index++;
-        print(index);
 
+
+
+        int temp = 1;
+        for (int i = 0; i < 4; i++)
+        {
+            activeLevels.Add(allLevels[Random.Range(0, 3)]);
+            switch (temp)
+            {
+                case 1:
+                    Instantiate(activeLevels[index], new Vector3(0, 0 + (activeLevels[index].transform.localScale.y * 0.25f) * index, 0), Quaternion.Euler(0f, -90f * index, 0f));
+                    temp++;
+                    break;
+
+                case 2:
+                    Instantiate(activeLevels[index], new Vector3(activeLevels[index].transform.localScale.x * 0.5f, 0 + (activeLevels[index].transform.localScale.y * 0.25f) * index, activeLevels[index].transform.localScale.x * 0.5f), Quaternion.Euler(0f, -90f * index, 0f));
+                    temp++;
+                    break;
+
+                case 3:
+                    print(temp);
+                    Instantiate(activeLevels[index], new Vector3(0, 0 + (activeLevels[index].transform.localScale.y * 0.25f) * index, 21), Quaternion.Euler(0f, -90f * index, 0f));
+                    temp++;
+                    break;
+
+                case 4:
+                    print(temp);
+                    Instantiate(activeLevels[index], new Vector3(-(activeLevels[index].transform.localScale.x) * 0.5f, 0 + (activeLevels[index].transform.localScale.y * 0.25f) * index, activeLevels[index].transform.localScale.x * 0.5f), Quaternion.Euler(0f, -90f * index, 0f));
+                    temp++;
+                    break;
+            }
+
+            if (temp % 5 == 0)
+                temp = 0;
+
+
+
+
+
+            index++;
+            print(index);
+        }
     }
 }
