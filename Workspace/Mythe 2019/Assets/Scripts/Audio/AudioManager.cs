f3dce7c59d45d7925ca73345327700d7393f4b87
JudaHensen <24311@ma-web.nl>
2019-03-11 15:12:34 +0100

    Added AudioManager - Still need more delegates
    Added Audio_Music
    - Need fade between switching themes

---
+++ Workspace/Mythe 2019/Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    // Delegates
+    public event Action<bool> Mute_Music, Mute_FX;
+    public event Action Change_Theme;
+
+    private bool Muted_Music_Status, Muted_FX_Status;
+    
+
+    void Start()
+    {
+        if (PlayerPrefs.HasKey("Audio_Muted_Music")) Boolean.TryParse(PlayerPrefs.GetString("Audio_Muted_Music"), out Muted_Music_Status);
+        else
+        {
+            PlayerPrefs.SetString("Audio_Muted_Music", "False");
+            Muted_Music_Status = false;
+        }
+
+        if (PlayerPrefs.HasKey("Audio_Muted_FX"))
+        {
+            if (PlayerPrefs.GetString("Audio_Muted_FX") == "True") Boolean.TryParse(PlayerPrefs.GetString("Audio_Muted_FX"), out Muted_FX_Status);
+        }
+        else
+        {
+            PlayerPrefs.SetString("Audio_Muted_FX", "False");
+            Muted_FX_Status = false;
+        }
+
+        Mute_Music(Muted_Music_Status);
+        Mute_FX(Muted_FX_Status);
+    }
+
+    public void MuteMusic()
+    {
+        // Change boolean to true if false, false if true
+        Muted_Music_Status = !Muted_Music_Status;
+        // Safe player settings
+        PlayerPrefs.SetString("Muted_Music_Status", Muted_Music_Status.ToString());
+        // Send delegate
+        Mute_Music(Muted_Music_Status);
+    }
+
+    public void MuteFX()
+    {
+        // Change boolean to true if false, false if true
+        Muted_FX_Status = !Muted_FX_Status;
+        // Safe player settings
+        PlayerPrefs.SetString("Audio_Muted_FX", Muted_FX_Status.ToString());
+        // Send delegate
+        Mute_FX(Muted_FX_Status);
+    }
+
+    public void ChangeTheme()
+    {
+        Change_Theme();
+    }
+}
---
+++ Workspace/Mythe 2019/Assets/Scripts/Audio/Audio_Music.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio_Music : MonoBehaviour
+{
+    private AudioManager audioManager;
+    private float volume;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        volume = GetComponent<AudioSource>().volume;
+        audioManager = GameObject.FindWithTag("AudioManager").GetComponent<AudioManager>();
+        audioManager.Mute_Music += Mute;
+        //audioManager.Change_Theme += ;
+    }
+
+    private void Mute(bool value)
+    {
+        if(value) GetComponent<AudioSource>().volume = 0;
+        else GetComponent<AudioSource>().volume = volume;
+    }
+
+
+
+}
