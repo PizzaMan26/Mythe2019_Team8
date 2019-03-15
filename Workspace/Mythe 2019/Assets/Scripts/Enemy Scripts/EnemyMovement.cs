e6915fd4e2c7a5e96c71228ed5b5319e5566a427
SMOOCHIE <justinudo959@gmail.com>
2019-03-15 11:05:24 +0100

    -Added: New scene with CameraMovement and Boss -Added: Boss

---
+++ Workspace/Mythe 2019/Assets/Scripts/Enemy Scripts/EnemyMovement.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    public GameObject target;
+
+    private Health _enemyHealth;
+
+
+
+    public int getEnemyLayer
+    {
+        get
+        {
+            return _enemyLayer;
+        }
+
+        set
+        {
+            _enemyLayer = value;
+        }
+    }
+
+    public float _enemySpeed;
+    private int _enemyLayer = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void CheckForPlayer()
+    {
+        if (_enemyLayer == 0)
+        {
+            Vector3 pos = new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z);
+
+            transform.position = Vector3.MoveTowards(transform.position, pos, _enemySpeed);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            CheckForPlayer();
+        }
+    }
+
+
+    private void RunAway()
+    {
+        
+    }
+}
