aaf0e4b3c6def5fcf6a17e398e10fb025e67d2a6
JudaHensen <24311@ma-web.nl>
2019-03-18 13:41:12 +0100

    Package Import: MainMenu Package Import due to possible merge errors.

---
+++ Workspace/Mythe 2019/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Load Game scene
+    public void Play()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    // Exit application
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+}
