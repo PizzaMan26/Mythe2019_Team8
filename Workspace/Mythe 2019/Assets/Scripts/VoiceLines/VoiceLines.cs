aeccac29fc5400af7a164d61fa1bd5b03c5226fa
JudaHensen <24311@ma-web.nl>
2019-03-19 13:26:53 +0100

    no message

---
+++ Workspace/Mythe 2019/Assets/Scripts/VoiceLines/VoiceLines.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoiceLines : MonoBehaviour
+{
+    enum Languages {Afrikaans, Arabic, Cat, Chinese, DonaldDuck, Duck, Dutch, English,
+                    French, Georgian, German, Goofie, Hungarian, Italiaans, Japanese,
+                    Korean, MickeyMouse, Norwegian, Polish, Portugese, Russian, Spanish,
+                    Thai, Vietnamese};
+
+
+
+}
