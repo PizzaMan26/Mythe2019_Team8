a9f30cb0fe25011d0295f92744a10803a79dfb9d
SMOOCHIE <justinudo959@gmail.com>
2019-03-21 09:28:21 +0100

    -Added: VoiceTrigger script (if conditions are met, enemies should talk) -Added: Joran's health script (didn't have it earlier)

--- Workspace/Mythe 2019/Assets/Scripts/Health/Health.cs
+++ Workspace/Mythe 2019/Assets/Scripts/Health/Health.cs
@@ -1,41 +1,50 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Health : MonoBehaviour
 {
-    private int _health = 100;
+    public int health = 100;
+    public bool isPlayer = false;
     private Rigidbody2D _rb;
-    private GameObject GameManager;
+
+    private GameObject IF;
+
+    public event Action playerDead;
+    public event Action<int> playerHit;
 
     void Start()
     {
-        GameManager = GameObject.Find("GameManager");
         _rb = GetComponent<Rigidbody2D>();//get's the rigidbody2D
     }
 
     public void DealDamage(int damage)//deals damage to object
     {
-        _health = _health - damage;
-
-        if(_health <= 0)//wasted
+        if (!GetComponent<InvincibilityFrames>().isInvincible)
         {
+            health = health - damage;
+            if (isPlayer)
+            {
+                playerHit(health);
+            }
+        }
+        
+
+        if(health <= 0)//wasted
+        {
+            if (isPlayer)
+            {
+                playerDead();
+            } 
             Debug.Log("RIP in perpperonies");
         }
     }
 
-    public void OnHit(float force)//knockup for throwing in the air
+    public void OnHit(float force)//knockup for juggling
     {
         Vector2 push = new Vector2(Input.GetAxis("Horizontal"), force);
         _rb.AddForce(push);
-    }
-
-    public void Juggle(float amount)//resets velocity for juggling
-    {
-        if (GameManager.GetComponent<IsOnGround>().CheckOnGround(transform, 3.5f) == null)
-        {
-            _rb.velocity = new Vector2(0, amount);
-        }
     }
 
 
