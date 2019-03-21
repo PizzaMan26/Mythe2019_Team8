bc1a2ae559c156b30c5ba2636c8ab01cf14f7a22
killersshadow <32833605+killersshadow@users.noreply.github.com>
2019-03-21 09:03:17 +0100

    added AttackEnemy

---
+++ Workspace/Mythe 2019/Assets/Scripts/Attack/AttackEnemy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackEnemy : MonoBehaviour
+{
+    [SerializeField]
+    private int damage = 0;
+
+    private void OnTriggerEnter2D(Collider2D col)//send attack to the object that got hit
+    {
+        if (col.tag == "Player")
+        {
+            col.SendMessage("DealDamage", damage);
+        }
+    }
+}
