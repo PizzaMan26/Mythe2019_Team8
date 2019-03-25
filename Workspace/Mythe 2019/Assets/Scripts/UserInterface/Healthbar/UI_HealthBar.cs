e803cc826297a26232a7703ee2ef126f0ecf4199
JudaHensen <24311@ma-web.nl>
2019-03-25 14:33:37 +0100

    Started working on Healthbar

---
+++ Workspace/Mythe 2019/Assets/Scripts/UserInterface/Healthbar/UI_HealthBar.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Healthbar : MonoBehaviour
+{
+    /* Misschien kunnen we regelen dat de ruimte tussen de achterste
+     * bladzijde en het boek een var is, waardoor we een kleinbeetje kunnen
+     * zien hoeveel bladzijdes er nog ongeveer zijn.
+     * dan voor de teat animation maken we een animatie waarbij ze van beneden
+     * naar boven worden gescheurd, zodat we met code de bladzijdes naar boven
+     * kunnen bewegen, en dan langzaam kunnen vervagen
+     * 
+     */
+
+
+
+    private int maximumHealth, currentHealth;
+    private Text text;
+
+    public int MaximumHealth {
+        set {
+            currentHealth = value;
+        }
+    }
+
+
+    void Start()
+    {
+
+    }
+
+    void RemoveHealth()
+    {
+        PlayAnimation();
+        text.text = currentHealth + " / " + maximumHealth;
+    }
+
+    // Play page tear animation
+    void PlayAnimation()
+    {
+        
+    }
+}
