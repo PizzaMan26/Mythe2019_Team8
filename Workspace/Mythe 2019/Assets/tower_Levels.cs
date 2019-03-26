e02d60ffe022513336de76781e2947de8daf3b50
BradleyBekker <thekid72gaming@gmail.com>
2019-03-26 13:35:20 +0100

    added level design and other parts of the tower

---
+++ Workspace/Mythe 2019/Assets/tower_Levels.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tower_Levels : MonoBehaviour
+{
+    [SerializeField]private List<Tower_Switch_Sides> _towerParts = new List<Tower_Switch_Sides>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < _towerParts.Count; i++)
+        {
+            _towerParts[i].enabled = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
