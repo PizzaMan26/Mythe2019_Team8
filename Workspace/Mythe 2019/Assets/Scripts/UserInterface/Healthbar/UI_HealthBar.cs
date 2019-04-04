306a4c0311dc0ff63e0448d884729148943208bb
JudaHensen <24311@ma-web.nl>
2019-04-04 11:25:37 +0200

    Completed UI HealthBar, need to test it in the game scene after merge.

--- Workspace/Mythe 2019/Assets/Scripts/UserInterface/Healthbar/UI_HealthBar.cs
+++ Workspace/Mythe 2019/Assets/Scripts/UserInterface/Healthbar/UI_HealthBar.cs
@@ -5,31 +5,15 @@ using UnityEngine.UI;
 
 public class UI_HealthBar : MonoBehaviour
 {
-    /* Misschien kunnen we regelen dat de ruimte tussen de achterste
-     * bladzijde en het boek een var is, waardoor we een kleinbeetje kunnen
-     * zien hoeveel bladzijdes er nog ongeveer zijn.
-     * dan voor de teat animation maken we een animatie waarbij ze van beneden
-     * naar boven worden gescheurd, zodat we met code de bladzijdes naar boven
-     * kunnen bewegen, en dan langzaam kunnen vervagen
-     * 
-     */
+    [SerializeField]
+    private int maximumHealth;
 
+    private int currentHealth;
 
-
-    private int maximumHealth, currentHealth;
     [SerializeField]
     private Text txt_curHealth, txt_maxHealth;
 
     private Health playerHealth;
-
-    [SerializeField]
-    private float pageDelay;
-    private float pageTimer = 0;
-    private int pageAmount;
-
-    [SerializeField]
-    private GameObject page;
-
 
     public int MaximumHealth {
         set {
@@ -38,34 +22,17 @@ public class UI_HealthBar : MonoBehaviour
         }
     }
 
-
     void Start()
     {
         playerHealth = GameObject.FindWithTag("Player").GetComponent<Health>();
-        playerHealth.playerHit += RemoveHealth;
+        playerHealth.playerHit += SetHealth;
     }
 
-    void RemoveHealth(int amount)
+    void SetHealth(int amount)
     {
-        pageAmount += currentHealth - amount;
         currentHealth = amount;
         if (currentHealth < 0) currentHealth = 0;
         
         txt_curHealth.text = currentHealth.ToString();
-    }
-
-    void Update()
-    {
-        if(pageAmount > 0)
-        {
-            pageTimer += Time.deltaTime;
-            if(pageTimer > pageDelay)
-            {
-                pageTimer = 0;
-                pageAmount--;
-                // Instantiate page
-                Instantiate(page, transform.position, transform.rotation);
-            }
-        }
     }
 }
