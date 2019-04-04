d84b451091b7d071622411303ee473279f2059d5
BradleyBekker <thekid72gaming@gmail.com>
2019-04-04 10:01:22 +0200

    added a death plane slightly under the camera if the player hits it he dies

---
+++ Workspace/Mythe 2019/Assets/DeathPlane.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathPlane : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "Player")
+        {
+            print("jojo");
+            Health PH = collision.GetComponent<Health>();
+            PH.DealDamage(10000000);
+        }
+    }
+}
