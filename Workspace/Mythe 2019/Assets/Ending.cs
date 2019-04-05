a9afd956bc89d5f10dcb6e7ccb0c88adce9d5d33
BradleyBekker <thekid72gaming@gmail.com>
2019-04-05 12:10:45 +0200

    babnsL.DBNKAQFBKW

---
+++ Workspace/Mythe 2019/Assets/Ending.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Ending : MonoBehaviour
+{
+
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "Player")
+        {
+            doEndAnim();
+            StartCoroutine(End());
+        }
+    }
+
+    IEnumerator End()
+    {
+     
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene("MainMenu");
+
+    }
+
+    private void doEndAnim()
+    {
+        anim.SetTrigger("Thumb");
+    }
+}
