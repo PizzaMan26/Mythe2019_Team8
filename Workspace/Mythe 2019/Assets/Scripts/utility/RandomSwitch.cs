7de8d5769f2f32d46e01f1729b9725f1eabf38f9
Floris <Flomo123456789@gmail.com>
2019-04-18 08:30:59 +0100

    music plays and switches now and again.

---
+++ Workspace/Mythe 2019/Assets/Scripts/utility/RandomSwitch.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSwitch : MonoBehaviour
+{
+    private float timer;
+    private float goalTime;
+
+    public event Action Switch;
+
+    [SerializeField]
+    private float minGoal, maxGoal;
+
+    void Update()
+    {
+        //basic timer
+        timer += Time.deltaTime;
+        if (timer > goalTime)
+        {
+            NewGoal();
+            timer = 0;
+            Switch();            
+        }
+    }
+
+    // sets a new goal
+    void NewGoal()
+    {
+        goalTime = UnityEngine.Random.Range(minGoal, maxGoal);
+    }
+}
